a508f34431b935b81d2d115488bff1f51e3a5f8d
richy-did <j.munezero2@alustudent.com>
2026-03-15 17:51:43 +0200

    new changes on task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
     new_string = ""
     for char in my_string:
         if char != 'c' and char != 'C'
             new_string += char
     return new_string
-print(no_c("Best School"))  # Best Shool
-print(no_c("Chicago"))      # hiago
-print(no_c("C is fun!"))    #  is fun!
