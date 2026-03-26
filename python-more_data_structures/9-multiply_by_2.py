3d03d538c0af788b76aca9a4433ea98eb714e2fa
richy-did <j.munezero2@alustudent.com>
2026-03-26 15:43:10 +0200

    new chnages

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_value = {}
     for num in a_dictionary:
-        new_value[num] = a_dictionary[num] * 2 
-    return new_value 
+        new_value[num] = a_dictionary[num] * 2
+    return new_value
