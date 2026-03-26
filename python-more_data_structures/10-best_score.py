2a46632d382caa22b520cca021b7d72d70c3636b
richy-did <j.munezero2@alustudent.com>
2026-03-26 15:35:16 +0200

    removed white spaces

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -6,6 +6,6 @@ def best_score(a_dictionary):
     best_value = None
     for key, value in a_dictionary.items():
         if best_value is None or value > best_value:
-            best_value = value 
-            best_key = key 
+            best_value = value
+            best_key = key
     return best_key
