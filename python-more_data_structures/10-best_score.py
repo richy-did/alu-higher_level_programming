19e53b1405152b55a8067e0e3dbe6751a15c922d
richy-did <j.munezero2@alustudent.com>
2026-03-26 15:33:27 +0200

    made == to is

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     best_key = None
     best_value = None
     for key, value in a_dictionary.items():
-        if best_value == None or value > best_value:
+        if best_value is None or value > best_value:
             best_value = value 
             best_key = key 
     return best_key
