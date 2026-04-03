b34539beb16c36838abae529e669bdf03bf65c85
richy-did <j.munezero2@alustudent.com>
2026-04-03 14:39:21 +0200

    new changes

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """
-documenting strings
+Fetches the status from ALU intranet using requests
 """
 import requests
-
-
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    if url.startswith('https://'):
-        url = "https://alu-intranet.hbtn.io/status"
-    res = requests.get(url)
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
