24273b00b44efaf8db7b2254dd0e5aad43446391
richy-did <j.munezero2@alustudent.com>
2026-06-14 20:48:31 +0200

    new changes

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,8 +4,6 @@ function factorial(n) {
   if (isNaN(n) || n <= 1) {
     return 1;
   }
-
   return n * factorial(n - 1);
 }
-
 console.log(factorial(parseInt(process.argv[2])));
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,5 +3,4 @@
 function add(a, b) {
   return a + b;
 }
-
 console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
